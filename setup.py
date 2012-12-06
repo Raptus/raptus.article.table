d5d9538af0637098ae18747f3a59e47dffad1e28
Simon Käser <skaeser@raptus.com>
2012-12-06 12:23:56 +0100

    Update setup.py
    Added dependency for simplejson

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(name='raptus.article.table',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'simplejson',
           'archetypes.schemaextender',
           'raptus.article.core',
           # -*- Extra requirements: -*-
